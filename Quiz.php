a9ce6957929668639f98e270fee3f622979d3ac5
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-04-22 13:26:24 +0200

    Quiz 5

--- Quiz.php
+++ Quiz.php
@@ -1,21 +1,27 @@
 <?php
+
 //clear terminal
 echo "\e[H\e[J";
+
 //genereert random getal tussen 1, 100
 $random = rand(1,100);
 
 echo "Type 'stop' om te stoppen\n";
+
 //vraagt invoer
 $invoer = readline("Raad het getal tussen de 1 en 100: ");
 echo "\n";
+
 //checkt of invoer == $random
 if ($invoer == $random) {
     exit("Het anwoord is goed!");
 }
+
 //checkt of invoer == "stop"
 if ($invoer == "stop") {
     exit("Het programma is gestopt\n");
 }
+
 //als invoer != $random of "stop" dan runt deze loop. Deze loop blijft runnen als $invoer != $random
 while ($invoer != $random) {
 //checkt het aantal karakters en limiteerd deze naar 3. Checkt dan of invoer een integer is.
@@ -37,9 +43,11 @@ while ($invoer != $random) {
         echo "Het getal is kleiner dan $invoer\n";
         echo "\n";
     }
+
 //vraagt nieuwe invoer op
     $invoer = readline("Probeer het opnieuw: ");
     echo "\n";
+    
 //stopt programma als $invoer == "stop"
     if ($invoer == "stop") {
         exit("Het programma is gestopt\n");
