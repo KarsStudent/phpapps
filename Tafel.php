ff878157dc71b6f21401e3a65a00f64d21df6b45
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-04-23 21:40:14 +0200

    Opschoning

--- Tafel.php
+++ Tafel.php
@@ -6,18 +6,6 @@ $tafel=readline("Welke tafel wil je zien? ");
 $begin=1;
 $eind=10;
 
-if ($tafel == "je moeder") {
-    exit("Je moeder is geen tafel!");
-} else if ($tafel == "je vader") {
-    exit("Je vader is een sukkel!");
-} else if ($tafel == "naomi") {
-    exit("Naomi is een lieverd!");
-} else if ($tafel == "kars") {
-    exit("karsie parsie is een snarsie");
-} else if ($tafel == "ballon") {
-    exit ("up up it goes");
-}
-
 echo "\n\nTafel van $tafel:\n";
 
 for ($teller=$begin;$teller<=$eind;$teller++) {
