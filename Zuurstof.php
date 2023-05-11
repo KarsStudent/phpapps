01a042e005ff6c40cbd5fae5800660630f363a19
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-05-11 18:40:07 +0200

    Zuurstof.php 4

--- Zuurstof.php
+++ Zuurstof.php
@@ -7,17 +7,16 @@ $b = readline("Druk in bar: ");
 $lpm = readline("Benodigd aantal liter per minuut: ");
 
 $m = $L * $b / $lpm;
-$rest = $m - floor($m);
 
 if ($m <= 60) {
-    $m = number_format($m, 2, ',');
+    $m = number_format($m, 0, ',');
     echo "\nJe hebt nog voor $m minuten zuurstof. :)\n";
 }
 
 if ($m > 60) {
-    $hours = $m / 60;
+    $hours = floor($m / 60);  
     $minutes = $m % 60;
-
+    
     echo "$hours uur en $minutes minuten. :)";
 }
 
