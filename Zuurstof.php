3a4bc66a329861af94a8e62f0e2a4a18a7f724fd
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-05-11 13:48:28 +0200

    Zuurstof.php 2

--- Zuurstof.php
+++ Zuurstof.php
@@ -6,8 +6,7 @@ $L = readline("Cilinder inhoud in liter: ");
 $b = readline("Druk in bar: ");
 $lpm = readline("Benodigd aantal liter per minuut:");
 
-$m = $L * $b / $lpm;
-
+$m = number_format($L * $b / $lpm, 2, ',');
 echo "\n$m minuten";
 
 ?>
