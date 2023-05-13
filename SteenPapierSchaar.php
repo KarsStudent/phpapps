b2791d01f7e9f4aff2fb69b415bcef6433671ac2
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-05-13 14:30:48 +0200

    SteenPapierSchaar.php 2

--- SteenPapierSchaar.php
+++ SteenPapierSchaar.php
@@ -1,19 +1,47 @@
 <?php
 
-$elements = array("steen", "papier", "schaar");
+//Clear terminal
+echo "\e[H\e[J";
+
+//Random element kiezen
+$elements = array("steen", "schaar", "papier");
 $random = rand(0, 2);
 
-echo $elements[$random];
-echo "\n";
+//Vraag input en check of deze goed is
+for ($i = 0; $i <= 0; $i++) {
 
-$input = readline("Steen, papier of schaar? ");
+    $input = readline("Steen, papier of schaar? ");
 
-if ($input == $elements[$random]) {
-    echo "Goed!";
-} else {
-    echo "Helaas...";
+    if (!in_array($input, $elements)) {
+        $i--;
+    }
 }
 
+//Check of user wint, verliest of dat het gelijkspel is
+if ($input == $elements[$random]) {
 
+    echo ("\nHet is gelijkspel! Jij koos: $input\n");
+    echo ("De computer koos: ".$elements[$random]);
+
+} else if ($random == 0){
+
+    if ($input == $elements[$random + 2]) {
+        echo ("\nJe hebt gewonnen! Jij koos: $input\n");
+        echo ("De computer koos: ".$elements[$random]);
+    } else {
+        echo "\nHelaas, je hebt niet gewonnen. Jij koos: $input\n";
+        echo ("De computer koos: ".$elements[$random]);
+    }
+
+} else if ($random >= 0) {
+
+    if ($input == $elements[$random - 1]) {
+        echo ("\nJe hebt gewonnen! Jij koos: $input\n");
+        echo ("De computer koos: ".$elements[$random]);
+    } else {
+        echo "\nHelaas, je hebt niet gewonnen. Jij koos: $input\n";
+        echo ("De computer koos: ".$elements[$random]);
+    }
+}
 
 ?>
