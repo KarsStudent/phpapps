f82741bfabffc05df272f4a22c97719752b1bff3
eJwtzE2PgjAUheH/0jULBNHS7WSiCUt1Mrum9N5CQ6XNLeBX/O82ht3JSZ73xZTWGOPZDzgywQxiXu23YDjnCgGwrncbU/E8rwsoNkbrXc2hApaxQH6xgLTS11qS05rqei+r/6X5baDpjd4u56UbIPxcejwcy7b8uxdPHctTSn2JnB4Bk2tREVJ6o/bfQ8HVjiLMrbNaDvjICBUIT10awWdzRBJ4VdZlN0+Dcf6WMN6DJYxSTUyMs3Pv9wcAJlEW <97106725@st.deltion.nl>
2023-05-26 10:28:23 +0200

    TekstDoorlopen.php 2

--- TekstDoorlopen.php
+++ TekstDoorlopen.php
@@ -1,40 +1,54 @@
 <?php
 
+echo "\e[H\e[J";
+
 $input = readline("Input: ");
-$spaces = 0;
-$sentences = 0;
-$klinker = 0;
-$medeklinker = 0;
-$leestekens = 0;
+$aantalSpaties = 0;
+$aantalZinnen = 0;
+$aantalKlinkers = 0;
+$aantalMedeklinkers = 0;
+$aantalLeestekens = 0;
+
+$klinkers = array("a", "e", "u", "o", "i", "A", "E", "U", "O", "I");
+$zinDefiner = array(".", "!", "?");
+$leestekens = array(".", ",", "!", "?", ":", ";");
+
+while (strlen($input) > 250) {
+    $input = readline("Input: ");
+}
 
 for ($i = 0; $i < strlen($input); $i++) {
     $char = $input[$i];
 
     if ($char == " ") {
-        $spaces++;
+        $aantalSpaties++;
     }
     
-    if ($char == "." || $char == "!" || $char == "?") {
-        $sentences++;
+    if (in_array($char, $zinDefiner)) {
+        $aantalZinnen++;
     }
 
-    if ($char == "a" || $char == "e" || $char == "u" || $char == "o" || $char == "i") {
-        $klinker++;
+    if (in_array($char, $klinkers)) {
+        $aantalKlinkers++;
     } else {
-        $medeklinker++;
+        $aantalMedeklinkers++;
     }
 
-    if ($char == "." || $char == "," || $char == ":" || $char == ";" || $char == "!" || $char == "?") {
-        $leestekens++;
+    if ($char == " " || in_array($char, $leestekens)) {
+        $aantalMedeklinkers--;
+    }
+
+    if (in_array($char, $leestekens)) {
+        $aantalLeestekens++;
     }
 }
 
 echo ("Aantal karakters: ".strlen($input)."\n");
 echo ("Aantal woorden: ".str_word_count($input)."\n");
-echo ("Aantal spaties: $spaces\n");
-echo ("Aantal zinnen: $sentences\n");
-echo ("Aantal klinkers: $klinker\n");
-echo ("Aantal medeklinkers: $medeklinker\n");
-echo ("Aantal leestekens: $leestekens");
+echo ("Aantal spaties: $aantalSpaties\n");
+echo ("Aantal zinnen: $aantalZinnen\n");
+echo ("Aantal klinkers: $aantalKlinkers\n");
+echo ("Aantal medeklinkers: $aantalMedeklinkers\n");
+echo ("Aantal leestekens: $aantalLeestekens");
 
 ?>
